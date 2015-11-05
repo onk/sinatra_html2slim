ba49f4ed8475a4e04ac92c07a88314584c2d263e
Takafumi ONAKA <takafumi.onaka@gmail.com>
2015-11-06 02:22:10 +0900

    add Sinatra::Reloader for development

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source "https://rubygems.org"
 gem "sinatra", require: "sinatra/base"
+gem "sinatra-contrib"
 
 group :development do
   gem "pry"
--- app.rb
+++ app.rb
@@ -1,4 +1,8 @@
 class App < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+  end
+
   get "/" do
     "Hello, World!"
   end
