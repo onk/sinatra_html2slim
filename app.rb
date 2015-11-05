aded4ee7abb5bf13237a56a1a2c7f9191e901ea3
Takafumi ONAKA <takafumi.onaka@gmail.com>
2015-11-06 06:43:52 +0900

    post redirect get

--- app.rb
+++ app.rb
@@ -4,11 +4,18 @@ class App < Sinatra::Base
     register Sinatra::Reloader
   end
 
+  enable :sessions
+
   route :get, :post, "/" do
-    @source = params[:source]
-    if @source
-      @target = HTML2Slim::HTMLConverter.new(params[:source]).to_s
+    if params[:source]
+      session[:source] = params[:source]
     end
+    if session[:source]
+      session[:target] = HTML2Slim::HTMLConverter.new(session[:source]).to_s
+      redirect to("/") if request.post?
+    end
+    @source = session[:source]
+    @target = session[:target]
     slim :index
   end
 end
